2784e0e03699bcf7fe5bced4ee9fa5ebe752eaa1
Sid <SidBayeck@outlook.com>
2023-08-19 16:09:20 +0800

    Initial Commit

---
+++ early-init.el
@@ -0,0 +1,71 @@
+;;; early-init.el --- Early initialization -*- lexical-binding: t -*-
+;;; Commentary;
+;;; Code:
+
+;; Defer garbage collection futher back in the startup process
+(setq gc-cons-threshold most-positive-fixnum)
+
+;; In noninteractive sessions, prioritize non-byte-compiled source files to
+;; prevent the use of stale byte-code. Otherwise, it saves us a little IO time
+;; to skip the mtime checks on every *.elc file.
+(setq load-prefer-newer noninteractive)
+
+;; Faster to disable these here (before they've been initialized)
+(setq default-frame-alist
+      (append
+       '((menu-bar-lines . 0)
+         (tool-bar-lines . 0)
+         (vertical-scroll-bars))
+       (when (featurep 'ns)
+         '((ns-transparent-titlebar . t)))
+       default-frame-alist))
+
+;;
+;;; Display
+(unless noninteractive
+  (let ((old-file-name-handler-alist file-name-handler-alist))
+    ;; `file-name-handler-alist' is consulted on each `require', `load' and
+    ;; various path/io functions. You get a minor speed up by unsetting this.
+    ;; Some warning, however: this could cause problems on builds of Emacs where
+    ;; its site lisp files aren't byte-compiled and we're forced to load the
+    ;; *.el.gz files (e.g. on Alpine).
+    (setq-default file-name-handler-alist nil)
+    ;; ...but restore `file-name-handler-alist' later, because it is needed for
+    ;; handling encrypted or compressed files, among other things.
+    (defun jcs--reset-file-handler-alist-h ()
+      (setq file-name-handler-alist
+            ;; Merge instead of overwrite because there may have bene changes to
+            ;; `file-name-handler-alist' since startup we want to preserve.
+            (delete-dups (append file-name-handler-alist
+                                 old-file-name-handler-alist))))
+    (add-hook 'emacs-startup-hook #'jcs--reset-file-handler-alist-h 101))
+
+  ;; Site files tend to use `load-file', which emits "Loading X..." messages in
+  ;; the echo area, which in turn triggers a redisplay. Redisplays can have a
+  ;; substantial effect on startup times and in this case happens so early that
+  ;; Emacs may flash white while starting up.
+  (define-advice load-file (:override (file) silence)
+    (load file nil 'nomessage))
+
+  ;; Undo our `load-file' advice above, to limit the scope of any edge cases it
+  ;; may introduce down the road.
+  (define-advice startup--load-user-init-file (:before (&rest _) init-jcs)
+    (advice-remove #'load-file #'load-file@silence)))
+
+;;
+;;; Bootstrap
+
+;; Contrary to what many Emacs users have in their configs, you don't need
+;; more than this to make UTF-8 the default coding system:
+(set-language-environment "UTF-8")
+
+;; set-language-enviornment sets default-input-method, which is unwanted
+(setq default-input-method nil)
+
+;;
+;;; Set Custom file
+(setq-default custom-file (concat user-emacs-directory "custom.el"))
+(when (file-exists-p custom-file) (load custom-file))
+
+(provide 'early-init)
+;;; early-init.el ends here
---
+++ init.el
@@ -0,0 +1,37 @@
+;;; init.el --- Sid's Emacs configuration  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023  Sid
+
+;; Author: Sid <SidBayeck@outlook.com>
+;; Maintainer: Sid <SidBayeck@outlook.com>
+;; URL: 
+;; Version: 1.0.0
+;; Package-Requires: ((emacs "29.1"))
+;; Keywords: lisp config
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Sid's Emacs configuration
+;;
+
+;;; Code:
+
+;; Happy coding! ;)
+
+(provide 'init)
+;;; init.el ends here
